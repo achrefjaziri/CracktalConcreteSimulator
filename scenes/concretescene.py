6dd266ae85f968f226246d2b6757453e6d04354a
Martin Mundt <mundt@fias.uni-frankfurt.de>
2017-11-17 11:36:51 +0100

    refactor concretescene

--- scenes/concretescene.py
+++ scenes/concretescene.py
@@ -1,25 +1,23 @@
 import bpy
 import math
 import os
-import sys
-import numpy as np
-import colorsys
 
 from lib.scene import Scene
 from lib.crackshader import CrackShader
 from lib.mastershader import MasterShader
 
+
 class ConcreteScene(Scene):
-    def __init__(self, resolution, isRenderCracks):
-        # attributes of the scene used in setup need to be called befor parent init!
+    def __init__(self, resolution, is_cracked):
+        # attributes of the scene used in setup need to be called before parent init!
         # otherwise they are not known to the overrinding function!
         self.resolution = resolution
 
-        self.cracked = isRenderCracks
-        
+        self.is_cracked = is_cracked
+
         super(ConcreteScene, self).__init__()
 
-    #Override(Scene)
+    # Override(Scene)
     def _setup_camera(self):
         # add camera
         bpy.ops.object.camera_add()
@@ -27,129 +25,75 @@ class ConcreteScene(Scene):
         bpy.data.objects['Camera'].location = (0.0, -15.0, 2.0)
         # rotation of camera. x axis rotation is 105.125 degrees in this example
         bpy.data.objects['Camera'].rotation_euler = [105.125*math.pi/180, 0.0, 0.0]
-        # scale of camera usually unaltered.
+        # scale of camera usually unaltered
         bpy.data.objects['Camera'].scale = [1.0, 1.0, 1.0]
-        # by default camera type is perspective. uncomment below line for changing camera to orthographic.
-        #bpy.data.cameras['Camera'].type='ORTHO'     #PERSP for perspective. default orthographic scale is 7.314
         # focal length of camera
         bpy.data.cameras['Camera'].lens = 35.00
         # focal length unit
-        bpy.data.cameras['Camera'].lens_unit = 'MILLIMETERS' #FOV for field of view
-        # note: you can add camera presets like samsung galaxy s4.
-        # look at documentation and uncomment below line and modify accordinly. below line not tested
-        #bpy.ops.script.execute_preset(filepath="yourpathlocation")
+        bpy.data.cameras['Camera'].lens_unit = 'MILLIMETERS'
 
-    #Override(Scene)
+    # Override(Scene)
     def _setup_lighting(self):
-        # add lamp
+        # add light source
         bpy.ops.object.lamp_add(type='SUN')
-        # change sun location.
-        bpy.data.objects['Sun'].location = [0.0,-5.0,5.0]
-        bpy.data.objects['Sun'].rotation_euler = [59*math.pi/180, 0.0, 0.0]
-
-        # you can modify object names. example given below. uncomment if needed.
-        # if uncommented upcoming commands should also be modified accordingly
-        #bpy.data.lamps[0].name = 'Sun'
-        #bpy.data.objects['Sun'].name = 'Sun'
-        # you can add more light sources. uncomment below line for example light source
-        #bpy.ops.object.lamp_add(type='POINT')
-
+        # change sun location
+        bpy.data.objects['Sun'].location = [0.0, -5.0, 5.0]
         # change rotation of sun's direction vector.
         bpy.data.objects['Sun'].rotation_euler = [59*math.pi/180, 0.0, 0.0]
-        # in order to change color, strength of sun, we have to use nodes.
-        # also node editing is quite useful for designing layers to your rendering
-        bpy.data.lamps['Sun'].use_nodes = True
-        # in the above statement we used 'Lamp' instead of index 0.
-        # The default name of lamps is 'Lamp' as described in the above comments.
 
-        # set color and strength value of sun using nodes.
+        # use nodes for blackbody
+        bpy.data.lamps['Sun'].use_nodes = True
+
+        # set color from temperature and light intensity
         bpy.data.lamps['Sun'].node_tree.nodes.new("ShaderNodeBlackbody")
-        # use a blackbody emission node for the sun and set the temperature. (based on Planck's Law)
+        # use a blackbody emission node for the sun and set the temperature
         bpy.data.lamps['Sun'].node_tree.nodes['Blackbody'].inputs['Temperature'].default_value = 5500
         bpy.data.lamps['Sun'].node_tree.links.new(bpy.data.lamps['Sun'].node_tree.nodes['Blackbody'].outputs['Color'],
-                                                 bpy.data.lamps['Sun'].node_tree.nodes['Emission'].inputs['Color'])
+                                                  bpy.data.lamps['Sun'].node_tree.nodes['Emission'].inputs['Color'])
         bpy.data.lamps['Sun'].node_tree.nodes['Emission'].inputs['Strength'].default_value = 3.3
 
-
-    #Override(Scene)
+    # Override(Scene)
     def _setup_objects(self):
-        # add a base primitive mesh. in this case a plane mesh is added at origin
+        # add a base primitive mesh. in this case a plane mesh is added at the origin
         bpy.ops.mesh.primitive_plane_add(location=(0.0, -2.0, 5.5))
         # default object name is 'Plane'. or index 2 in this case
         # set scale and rotation
         bpy.data.objects['Plane'].scale = [6.275, 6.275, 6.275]
         bpy.data.objects['Plane'].rotation_euler = [105*math.pi/180, 0.0, 0.0]
 
-    #Override(Scene)
+    # Override(Scene)
     def _setup_shader(self, concrete=1):
-        albedoPath = os.path.join('concretedictionary/concrete' + str(concrete) + '/albedo' + str(concrete) + '.png')
-        roughnessPath = os.path.join('concretedictionary/concrete' + str(concrete) + '/roughness' + str(concrete) + '.png');
-        normalPath = os.path.join('concretedictionary/concrete' + str(concrete) + '/normal' + str(concrete) + '.png')
-        
-        
-        #print("Init crackshader");
-        #shadername = "concrete";
-        #shader = CrackShader(shadername, albedoPath, roughnessPath, normalPath, self.resolution);
-        #self.shaderDict[shadername] = shader;
-        #print("Done...");
+        albedo_path = os.path.join('concretedictionary/concrete' + str(concrete) + '/albedo'
+                                   + str(concrete) + '.png')
+        roughness_path = os.path.join('concretedictionary/concrete' + str(concrete) + '/roughness'
+                                      + str(concrete) + '.png')
+        normal_path = os.path.join('concretedictionary/concrete' + str(concrete) + '/normal'
+                                   + str(concrete) + '.png')
 
-        #print("Link shader to plane object");
-        #bpy.data.objects['Plane'].active_material = bpy.data.materials[shadername];
-        #print("Done...");
+        shadername = "concrete"
+        if self.is_cracked:
+            shader = CrackShader(shadername, albedo_path, roughness_path, normal_path, self.resolution)
+        else:
+            shader = MasterShader(shadername, albedo_path, roughness_path, normal_path)
 
-        #print("Apply shader to obj mesh");
-        #shader.applyTo("Plane");
-        #print("Done...");
+        self.shaderDict[shadername] = shader
 
-        
-        if(not self.cracked):
-            print("Init mastershader");
-            shadername = "concrete";
-            shader = MasterShader(shadername, albedoPath, roughnessPath, normalPath);
-            self.shaderDict[shadername] = shader;
-            print("Done...");
+        # Link shader to plane object
+        bpy.data.objects['Plane'].active_material = bpy.data.materials['concrete']
+        # TODO: sample shader image sources!
 
-            print("Link shader to plane object");
-            bpy.data.objects['Plane'].active_material = bpy.data.materials['concrete'];
-            print("Done...");
+        # Sample shader values
+        shader.sample_texture()
 
-            # TODO: sample shader image sources!
+        # Apply shader to obj mesh
+        shader.apply_to_blender_object("Plane")
 
-            print("Sample shader values");
-            shader.sample_texture();
-            print("Done...");
-            
-            print("Apply shader to obj mesh");
-            shader.apply_to_blender_object("Plane");
-            print("Done...");
-            
-        elif(self.cracked):
-            print("Init crackshader");
-            shadername = "concrete";
-            shader = CrackShader(shadername, albedoPath, roughnessPath, normalPath, self.resolution);
-            self.shaderDict[shadername] = shader;
-            print("Done...");
-
-            print("Link shader to plane object");
-            bpy.data.objects['Plane'].active_material = bpy.data.materials['concrete'];
-            print("Done...");
-
-            # TODO: sample shader image sources!
-
-            print("Sample shader values");
-            shader.sample_texture();
-            print("Done...");
-            
-            print("Apply shader to obj mesh");
-            shader.apply_to_blender_object("Plane");
-            print("Done...");
-        
-
-    #Override(Scene)
+    # Override(Scene)
+    # TODO: why is this construct necessary?
     def update(self):
         for key in self.shaderDict:
             try:
                 for key in self.shaderDict:
-                    self.shaderDict[key].sample_texture();
+                    self.shaderDict[key].sample_texture()
             except Exception:
                 pass;
